eb850c65719e2ba805fed6e72a3f92cd19c17be0
neo_cloud <1786546913@qq.com>
2018-03-07 21:15:21 +0800

    PYTHON

--- Deep_Learning_with_TensorFlow/1.4.0/Chapter04/2. 学习率的设置.py
+++ Deep_Learning_with_TensorFlow/1.4.0/Chapter04/2. 学习率的设置.py
@@ -1,13 +1,5 @@
-
-# coding: utf-8
-
 # #### 假设我们要最小化函数  $y=x^2$, 选择初始点   $x_0=5$
-
 # #### 1. 学习率为1的时候，x在5和-5之间震荡。
-
-# In[1]:
-
-
 import tensorflow as tf
 TRAINING_STEPS = 10
 LEARNING_RATE = 1
@@ -21,13 +13,9 @@ with tf.Session() as sess:
     for i in range(TRAINING_STEPS):
         sess.run(train_op)
         x_value = sess.run(x)
-        print "After %s iteration(s): x%s is %f."% (i+1, i+1, x_value) 
-
+        print("After %s iteration(s): x%s is %f."% (i+1, i+1, x_value))
 
 # #### 2. 学习率为0.001的时候，下降速度过慢，在901轮时才收敛到0.823355。
-
-# In[2]:
-
 
 TRAINING_STEPS = 1000
 LEARNING_RATE = 0.001
@@ -42,13 +30,8 @@ with tf.Session() as sess:
         sess.run(train_op)
         if i % 100 == 0: 
             x_value = sess.run(x)
-            print "After %s iteration(s): x%s is %f."% (i+1, i+1, x_value)
-
-
+            print("After %s iteration(s): x%s is %f."% (i+1, i+1, x_value))
 # #### 3. 使用指数衰减的学习率，在迭代初期得到较高的下降速度，可以在较小的训练轮数下取得不错的收敛程度。
-
-# In[3]:
-
 
 TRAINING_STEPS = 100
 global_step = tf.Variable(0)
@@ -65,5 +48,36 @@ with tf.Session() as sess:
         if i % 10 == 0:
             LEARNING_RATE_value = sess.run(LEARNING_RATE)
             x_value = sess.run(x)
-            print "After %s iteration(s): x%s is %f, learning rate is %f."% (i+1, i+1, x_value, LEARNING_RATE_value)
+            print("After %s iteration(s): x%s is %f, learning rate is %f."% (i+1, i+1, x_value, LEARNING_RATE_value))
 
+
+# After 1 iteration(s): x1 is -5.000000.
+# After 2 iteration(s): x2 is 5.000000.
+# After 3 iteration(s): x3 is -5.000000.
+# After 4 iteration(s): x4 is 5.000000.
+# After 5 iteration(s): x5 is -5.000000.
+# After 6 iteration(s): x6 is 5.000000.
+# After 7 iteration(s): x7 is -5.000000.
+# After 8 iteration(s): x8 is 5.000000.
+# After 9 iteration(s): x9 is -5.000000.
+# After 10 iteration(s): x10 is 5.000000.
+# After 1 iteration(s): x1 is 4.990000.
+# After 101 iteration(s): x101 is 4.084646.
+# After 201 iteration(s): x201 is 3.343555.
+# After 301 iteration(s): x301 is 2.736923.
+# After 401 iteration(s): x401 is 2.240355.
+# After 501 iteration(s): x501 is 1.833880.
+# After 601 iteration(s): x601 is 1.501153.
+# After 701 iteration(s): x701 is 1.228794.
+# After 801 iteration(s): x801 is 1.005850.
+# After 901 iteration(s): x901 is 0.823355.
+# After 1 iteration(s): x1 is 4.000000, learning rate is 0.096000.
+# After 11 iteration(s): x11 is 0.690561, learning rate is 0.063824.
+# After 21 iteration(s): x21 is 0.222583, learning rate is 0.042432.
+# After 31 iteration(s): x31 is 0.106405, learning rate is 0.028210.
+# After 41 iteration(s): x41 is 0.065548, learning rate is 0.018755.
+# After 51 iteration(s): x51 is 0.047625, learning rate is 0.012469.
+# After 61 iteration(s): x61 is 0.038558, learning rate is 0.008290.
+# After 71 iteration(s): x71 is 0.033523, learning rate is 0.005511.
+# After 81 iteration(s): x81 is 0.030553, learning rate is 0.003664.
+# After 91 iteration(s): x91 is 0.028727, learning rate is 0.002436.
--- Deep_Learning_with_TensorFlow/1.4.0/Chapter04/3. 正则化.py
+++ Deep_Learning_with_TensorFlow/1.4.0/Chapter04/3. 正则化.py
@@ -1,42 +1,31 @@
-
-# coding: utf-8
-
 # #### 1. 生成模拟数据集。
-
-# In[1]:
-
-
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
 data = []
 label = []
-np.random.seed(0)
+np.random.seed(0)  # 将0作为seed种子值
 
 # 以原点为圆心，半径为1的圆把散点划分成红蓝两部分，并加入随机噪音。
 for i in range(150):
-    x1 = np.random.uniform(-1,1)
-    x2 = np.random.uniform(0,2)
-    if x1**2 + x2**2 <= 1:
-        data.append([np.random.normal(x1, 0.1),np.random.normal(x2,0.1)])
+    x1 = np.random.uniform(-1, 1)
+    x2 = np.random.uniform(0, 2)
+    if x1 ** 2 + x2 ** 2 <= 1:
+        data.append([np.random.normal(x1, 0.1), np.random.normal(x2, 0.1)])
         label.append(0)
     else:
         data.append([np.random.normal(x1, 0.1), np.random.normal(x2, 0.1)])
         label.append(1)
-        
-data = np.hstack(data).reshape(-1,2)
+
+data = np.hstack(data).reshape(-1, 2)
 label = np.hstack(label).reshape(-1, 1)
-plt.scatter(data[:,0], data[:,1], c=label,
-           cmap="RdBu", vmin=-.2, vmax=1.2, edgecolor="white")
+plt.scatter(data[:, 0], data[:, 1], c=label,
+            cmap="RdBu", vmin=-.2, vmax=1.2, edgecolor="white")
 plt.show()
 
 
 # #### 2. 定义一个获取权重，并自动加入正则项到损失的函数。
-
-# In[2]:
-
-
 def get_weight(shape, lambda1):
     var = tf.Variable(tf.random_normal(shape), dtype=tf.float32)
     tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(lambda1)(var))
@@ -44,16 +33,12 @@ def get_weight(shape, lambda1):
 
 
 # #### 3. 定义神经网络。
-
-# In[3]:
-
-
 x = tf.placeholder(tf.float32, shape=(None, 2))
 y_ = tf.placeholder(tf.float32, shape=(None, 1))
 sample_size = len(data)
 
 # 每层节点的个数
-layer_dimension = [2,10,5,3,1]
+layer_dimension = [2, 10, 5, 3, 1]
 
 n_layers = len(layer_dimension)
 
@@ -68,19 +53,14 @@ for i in range(1, n_layers):
     cur_layer = tf.nn.elu(tf.matmul(cur_layer, weight) + bias)
     in_dimension = layer_dimension[i]
 
-y= cur_layer
+y = cur_layer
 
 # 损失函数的定义。
-mse_loss = tf.reduce_sum(tf.pow(y_ - y, 2)) / sample_size
+mse_loss = tf.reduce_sum(tf.pow(y_ - y, 2))/sample_size
 tf.add_to_collection('losses', mse_loss)
 loss = tf.add_n(tf.get_collection('losses'))
 
-
 # #### 4. 训练不带正则项的损失函数mse_loss。
-
-# In[4]:
-
-
 # 定义训练的目标函数mse_loss，训练次数及训练模型
 train_op = tf.train.AdamOptimizer(0.001).minimize(mse_loss)
 TRAINING_STEPS = 40000
@@ -89,26 +69,21 @@ with tf.Session() as sess:
     tf.global_variables_initializer().run()
     for i in range(TRAINING_STEPS):
         sess.run(train_op, feed_dict={x: data, y_: label})
-        if i % 2000 == 0:
-            print("After %d steps, mse_loss: %f" % (i,sess.run(mse_loss, feed_dict={x: data, y_: label})))
+        if i%2000 == 0:
+            print("After %d steps, mse_loss: %f"%(i, sess.run(mse_loss, feed_dict={x: data, y_: label})))
 
     # 画出训练后的分割曲线       
     xx, yy = np.mgrid[-1.2:1.2:.01, -0.2:2.2:.01]
     grid = np.c_[xx.ravel(), yy.ravel()]
-    probs = sess.run(y, feed_dict={x:grid})
+    probs = sess.run(y, feed_dict={x: grid})
     probs = probs.reshape(xx.shape)
 
-plt.scatter(data[:,0], data[:,1], c=label,
-           cmap="RdBu", vmin=-.2, vmax=1.2, edgecolor="white")
+plt.scatter(data[:, 0], data[:, 1], c=label,
+            cmap="RdBu", vmin=-.2, vmax=1.2, edgecolor="white")
 plt.contour(xx, yy, probs, levels=[.5], cmap="Greys", vmin=0, vmax=.1)
 plt.show()
 
-
 # #### 5. 训练带正则项的损失函数loss。
-
-# In[5]:
-
-
 # 定义训练的目标函数loss，训练次数及训练模型
 train_op = tf.train.AdamOptimizer(0.001).minimize(loss)
 TRAINING_STEPS = 40000
@@ -117,17 +92,16 @@ with tf.Session() as sess:
     tf.global_variables_initializer().run()
     for i in range(TRAINING_STEPS):
         sess.run(train_op, feed_dict={x: data, y_: label})
-        if i % 2000 == 0:
-            print("After %d steps, loss: %f" % (i, sess.run(loss, feed_dict={x: data, y_: label})))
+        if i%2000 == 0:
+            print("After %d steps, loss: %f"%(i, sess.run(loss, feed_dict={x: data, y_: label})))
 
     # 画出训练后的分割曲线       
     xx, yy = np.mgrid[-1:1:.01, 0:2:.01]
     grid = np.c_[xx.ravel(), yy.ravel()]
-    probs = sess.run(y, feed_dict={x:grid})
+    probs = sess.run(y, feed_dict={x: grid})
     probs = probs.reshape(xx.shape)
 
-plt.scatter(data[:,0], data[:,1], c=label,
-           cmap="RdBu", vmin=-.2, vmax=1.2, edgecolor="white")
+plt.scatter(data[:, 0], data[:, 1], c=label,
+            cmap="RdBu", vmin=-.2, vmax=1.2, edgecolor="white")
 plt.contour(xx, yy, probs, levels=[.5], cmap="Greys", vmin=0, vmax=.1)
 plt.show()
-
