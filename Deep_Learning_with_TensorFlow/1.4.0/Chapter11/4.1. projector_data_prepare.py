0ea7d52a4056e5e53391a452a9bbd468175af7f5
neo_cloud <1786546913@qq.com>
2018-07-18 16:07:34 +0800

    update

--- Deep_Learning_with_TensorFlow/1.4.0/Chapter11/4.1. projector_data_prepare.py
+++ Deep_Learning_with_TensorFlow/1.4.0/Chapter11/4.1. projector_data_prepare.py
@@ -5,25 +5,31 @@ import numpy as np
 import os
 from tensorflow.examples.tutorials.mnist import input_data
 
-LOG_DIR = 'log'
+# PROJECTOR需要的日志文件名和地址相关参数
+LOG_DIR = 'log_2'
 SPRITE_FILE = 'mnist_sprite.jpg'
 META_FIEL = "mnist_meta.tsv"
 
 
+# 使用给出的MNIST图片列表生成sprite图像
 def create_sprite_image(images):
     """Returns a sprite image consisting of images passed as argument. Images should be count x width x height"""
     if isinstance(images, list):
         images = np.array(images)
     img_h = images.shape[1]
     img_w = images.shape[2]
+    # sprite图像可以理解成是小图片平成的大正方形矩阵，大正方形矩阵中的每一个元素就是原来的小图片。于是这个正方形的边长就是sqrt(n),其中n为小图片的数量。
     n_plots = int(np.ceil(np.sqrt(images.shape[0])))
 
+    # 使用全1来初始化最终的大图片。
     spriteimage = np.ones((img_h*n_plots, img_w*n_plots))
 
     for i in range(n_plots):
         for j in range(n_plots):
+            # 计算当前图片的编号
             this_filter = i*n_plots + j
             if this_filter < images.shape[0]:
+                # 将当前小图片的内容复制到最终的sprite图像
                 this_img = images[this_filter]
                 spriteimage[i*img_h:(i + 1)*img_h,
                 j*img_w:(j + 1)*img_w] = this_img
@@ -31,17 +37,20 @@ def create_sprite_image(images):
     return spriteimage
 
 
+# 加载MNIST数据。这里指定了one_hot=False,于是得到的labels就是一个数字，表示当前图片所表示的数字。
 mnist = input_data.read_data_sets("../../datasets/MNIST_data", one_hot=False)
 
+# 生成sprite图像
 to_visualise = 1 - np.reshape(mnist.test.images, (-1, 28, 28))
 sprite_image = create_sprite_image(to_visualise)
 
+# 将生成的sprite图片放到相应的日志目录下
 path_for_mnist_sprites = os.path.join(LOG_DIR, SPRITE_FILE)
 plt.imsave(path_for_mnist_sprites, sprite_image, cmap='gray')
 plt.imshow(sprite_image, cmap='gray')
 
+# 生成每张图片对应的标签文件并写道相应的日志目录下
 path_for_mnist_metadata = os.path.join(LOG_DIR, META_FIEL)
-
 with open(path_for_mnist_metadata, 'w') as f:
     f.write("Index\tLabel\n")
     for index, label in enumerate(mnist.test.labels):
--- Deep_Learning_with_TensorFlow/1.4.0/Chapter11/4.2. projector_MNIST.py
+++ Deep_Learning_with_TensorFlow/1.4.0/Chapter11/4.2. projector_MNIST.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 import mnist_inference
 import os
+import tqdm
 
+# 加载用于生成PROJECTOR日志的帮助函数
 from tensorflow.contrib.tensorboard.plugins import projector
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -12,12 +14,13 @@ REGULARIZATION_RATE = 0.0001
 TRAINING_STEPS = 10000
 MOVING_AVERAGE_DECAY = 0.99
 
-LOG_DIR = 'log'
+LOG_DIR = 'log_2'
 SPRITE_FILE = 'mnist_sprite.jpg'
 META_FIEL = "mnist_meta.tsv"
 TENSOR_NAME = "FINAL_LOGITS"
 
 
+# 这里需要返回最后测试数据经过整个神经网络得到的输出层矩阵，因为有多张测试图片，每张图片对应了一个输出层向量。所以返回的结果是这些向量组成的矩阵。
 def train(mnist):
     #  输入数据的命名空间。
     with tf.name_scope('input'):
@@ -26,74 +29,96 @@ def train(mnist):
     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
     y = mnist_inference.inference(x, regularizer)
     global_step = tf.Variable(0, trainable=False)
-    
+
     # 处理滑动平均的命名空间。
     with tf.name_scope("moving_average"):
         variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
         variables_averages_op = variable_averages.apply(tf.trainable_variables())
-   
+
     # 计算损失函数的命名空间。
     with tf.name_scope("loss_function"):
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
         cross_entropy_mean = tf.reduce_mean(cross_entropy)
+        # 由于使用L2正则化，此处需要加上'losses'集合
         loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
-    
+
     # 定义学习率、优化方法及每一轮执行训练的操作的命名空间。
     with tf.name_scope("train_step"):
         learning_rate = tf.train.exponential_decay(
             LEARNING_RATE_BASE,
             global_step,
-            mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY,
+            mnist.train.num_examples/BATCH_SIZE, LEARNING_RATE_DECAY,
             staircase=True)
 
         train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-
+        # 由于使用了滑动平均方法，所以在反向传播时也要更新可训练变量的滑动平均值
         with tf.control_dependencies([train_step, variables_averages_op]):
             train_op = tf.no_op(name='train')
-    
+
     # 训练模型。
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
-        for i in range(TRAINING_STEPS):
+        for i in tqdm.tqdm(range(TRAINING_STEPS)):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
             _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
-                
-            if i % 1000 == 0:
-                print("After %d training step(s), loss on training batch is %g." % (i, loss_value))                
+
+            if i%1000 == 0:
+                print("After %d training step(s), loss on training batch is %g."%(i, loss_value))
+        # 计算MNIST测试数据对应的输出层矩阵
         final_result = sess.run(y, feed_dict={x: mnist.test.images})
-    
+    # 返回输出层矩阵的值
     return final_result
 
+
+# 生成可视化最终输出层向量所需要的日志文件
 def visualisation(final_result):
-    y = tf.Variable(final_result, name = TENSOR_NAME)
+    # 使用一个新的变量来保存最终输出层向量的结果，因为embedding是通过Tensorflow中变量完成的，所以PROJECTOR可视化的都是TensorFlow中的变哇。
+    # 所以这里需要新定义一个变量来保存输出层向量的取值
+    y = tf.Variable(final_result, name=TENSOR_NAME)
     summary_writer = tf.summary.FileWriter(LOG_DIR)
 
+    # 通过project.ProjectorConfig类来帮助生成日志文件
     config = projector.ProjectorConfig()
+    # 增加一个需要可视化的bedding结果
     embedding = config.embeddings.add()
+    # 指定这个embedding结果所对应的Tensorflow变量名称
     embedding.tensor_name = y.name
 
     # Specify where you find the metadata
+    # 指定embedding结果所对应的原始数据信息。比如这里指定的就是每一张MNIST测试图片对应的真实类别。在单词向量中可以是单词ID对应的单词。
+    # 这个文件是可选的，如果没有指定那么向量就没有标签。
     embedding.metadata_path = META_FIEL
 
     # Specify where you find the sprite (we will create this later)
+    # 指定sprite 图像。这个也是可选的，如果没有提供sprite 图像，那么可视化的结果
+    # 每一个点就是一个小困点，而不是具体的图片。
     embedding.sprite.image_path = SPRITE_FILE
-    embedding.sprite.single_image_dim.extend([28,28])
+    # 在提供sprite图像时，通过single_image_dim可以指定单张图片的大小。
+    # 这将用于从sprite图像中截取正确的原始图片。
+    embedding.sprite.single_image_dim.extend([28, 28])
 
     # Say that you want to visualise the embeddings
+    # 将PROJECTOR所需要的内容写入日志文件。
     projector.visualize_embeddings(summary_writer, config)
-    
+
+    # 生成会话，初始化新声明的变量并将需要的日志信息写入文件。
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()
     saver.save(sess, os.path.join(LOG_DIR, "model"), TRAINING_STEPS)
-    
+
     summary_writer.close()
 
-def main(argv=None): 
+
+# 主函数先调用模型训练的过程，再使用训练好的模型来处理MNIST测试数据，
+# 最后将得到的输出层矩阵输出到PROJECTOR需要的日志文件中。
+def main(argv=None):
     mnist = input_data.read_data_sets("../../datasets/MNIST_data", one_hot=True)
     final_result = train(mnist)
     visualisation(final_result)
 
+
 if __name__ == '__main__':
     main()
 
+# tensorboard --logdir=D:\CODE\Git\TensorFlow_Google_Practice\Deep_Learning_with_TensorFlow\1.4.0\Chapter11\log_2
